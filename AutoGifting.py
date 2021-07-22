d2b1998df7df00a16e30af798ef63e0faa4d44ea
Adler <adlerademov@gmail.com>
2021-07-22 13:51:33 +0300

    Update AutoGifting.py
    Version created on 22 July 2021.

--- AutoGifting.py
+++ AutoGifting.py
@@ -1,12 +1,55 @@
-import time
+#
+#   LordsMobile AutoGifting with code
+#
+#######  IMPORTING THE LIBRARIES #######
+from lxml.cssselect import CSSSelector
+from urllib.request import urlopen
+import lxml.html
 import requests
+import time
+import sys
 
+###############################################################################################################################################
+################################## THE CSS SELECTOR WAY OF PARSING THE GUILD NAMES (without kingdoms) #########################################
+print("======================================================================================================================================")
+print("THE CSS SELECTOR WAY OF PARSING THE GUILD NAMES (without kingdoms)")
+#Parsing the guild page
+
+# To gift to OnE, uncomment this
+url =  "https://lordsmobilemaps.com/en/alliance/OnE%20UNITED"
+
+# To gift to DI2, uncomment this
+#url =  "https://lordsmobilemaps.com/en/alliance/District12"
+
+# To gift to d|E, uncomment this
+#url =  "https://lordsmobilemaps.com/en/alliance/The%20Devils%20Elite"
+
+response = requests.get(url, stream=True)
+response.raw.decode_content = True
+tree = lxml.html.parse(response.raw)
+
+#(Method 1) Taking the names of the players with lxml's CSS Selector module
+guild_players = CSSSelector('.tobtab2col div:nth-of-type(n+2) div:nth-of-type(2) a')
+guildmembers_as_list = [] #Since tuples are immutable (cannot append in tuples), this has to be a list (later we convert this list to a tuple)
+for names in guild_players(tree): #Placing the data into a list
+    guildmembers_as_list.append(names.text)
+
+#Other players, who will be gifted too
+with_respected_guildmembers = ["RaiseUrBoobs"]
+with_respected_guildmembers.extend(guildmembers_as_list) #Extending the list, so the respected members can be gifted too
+
+#Converting list to tuple
+guildmembers = tuple(with_respected_guildmembers)
+
+print(guildmembers) #Returns a tuple, containing all of the players in the guild
+print("Total players:",len(guildmembers))
+###############################################################################################################################################
+
+#Asking the user to enter the gift code
 giftcode = input("Please insert the GiftCode:")
 
-guild = ("RaiseUrBoobs", "Bad Juju 4 U", "Otermaster", "vanillebaer", "iTzLife", "ADLER", "scorpionn1", "Tccc", "SpikeyMan", "Ozkopf", "Monkey Butt", "Sizalael", "Lt Kong", "Boindil", "Avaloch II", "Felix Ripper", "drasko 111", "Sozer I", "killerferret", "Squeaks", "Inf4dayz", "BoendalI", "Wootootoo", "Dromadaire", "Bottom 14", "Munchee", "ItsJujuBltch", "CaptainPeace", "Babalon", "chumbeke", "littleguy12", "marky t4", "SSJ Goldbug", "Addo X", "ROGER250891", "Chipset", "KnuffelSammy", "Davaron", "Chaos Enigma", "Ligma Ween", "MilkLover", "The Crippler", "marky1704", "BabyDant", "scorpionn2", "zeeroo1", "Stan S", "rock8383", "Neptunetwo", "mmusic93", "Goldbug", "Crixilicious", "Vuggati", "Calhoun714", "music93", "Ambarhane", "Token Inmate", "S1eeper", "annie 96", "Loony Goony", "BoredOwl", "Mad4 6", "Fapperino", "funkykya", "BonesMcCoy", "Mine Fieldz", "LadyDayana", "Big Meanie", "MrScotty", "Goldbugs Dad", "Goldbugs Mom", "Sizzor", "Jasper2", "Wheatbug", "Metalbug", "Add Stone", "scorpionn4", "Warzone911", "marky1704 2", "Add Food", "Add Wood", "FoodPro Mini", "Add Ore")
-
-#Going through the whole list of players
-for player in guild:
+#Going through the whole list of players and submitting their names with the given gift code
+for player in guildmembers:
     print(player)
     time.sleep(1.5)
     
